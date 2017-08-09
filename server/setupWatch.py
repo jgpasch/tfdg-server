5dfe8c4086829039d3748f0b6978a32303e7be12
John Paschal <jgpasch@gmail.com>
2017-08-09 16:17:34 -0400

    updated setupWatch.py in order to use the library apiclient to make watch request

--- server/setupWatch.py
+++ server/setupWatch.py
@@ -3,12 +3,23 @@
 import os
 import json
 import requests
+from apiclient import discovery
 from oauth2client.file import Storage
 from oauth2client import client
 from oauth2client import tools
 import httplib2
 import uuid
 import time
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+CLIENT_SECRET_FILE = '/home/john/tfdg-server/config/client_secret.json'
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+APPLICATION_NAME = 'Drive API Python Quickstart'
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -28,6 +39,7 @@ def get_credentials():
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
+        print('im insdie here')
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
         if flags:
@@ -37,40 +49,29 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-
-def setupWatcher(at):
-
+def main():
   firebase_url = 'https://tfdg-175615.firebaseio.com/config.json'
   result = requests.get(firebase_url)
   file_id = result.json()['file_id']
 
-  request_url = 'https://www.googleapis.com/drive/v3/files/' + file_id + '/watch'
-
   now = time.time() * 1000
   # tomorrow = int(now) + 86000000
   expiration = int(now) + 120000
-  print(expiration)
 
-  headers = { "Content-Type": "application/json", "Authorization": "Bearer " + at }
   data = { "id": str(uuid.uuid4()),
            "type": "web_hook",
            "address": "https://bigspender.info/driveCallback",
            "expiration": expiration }
 
+  credentials = get_credentials()
+  http = credentials.authorize(httplib2.Http())
 
-  res = requests.post(request_url, data = json.dumps(data), headers = headers)
+  service = discovery.build('drive', 'v3', http=http)
+  res = service.files().watch(fileId=file_id, body=data).execute()
 
   f_write = open('/home/john/tfdg-server/server/watchResponse.txt', 'w')
-  f_write.write(json.dumps(res.json()))
+  f_write.write(json.dumps(res))
   f_write.close()
-
-def main():
-  f = open('/home/john/.credentials/tfdg-drive-api-creds.json', 'r')
-  data = json.load(f)
-  setupWatcher(data['access_token'])
-  f.close()
-
-
 
 if __name__ == '__main__':
   main()
